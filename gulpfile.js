d2062cd784e828835e56a9f26536d027be74eae0
Adria <30989245+Naivvi@users.noreply.github.com>
2017-09-20 14:05:10 +1200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
-'use strict';
+// 'use strict';
  
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    // .pipe(autoprefixer())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({stream:true}));
 });
+
+gulp.task('serve', function(){
+
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	})
+
+	gulp.watch('./scss/**/*.scss', ['sass']);
+	gulp.watch('./**/*.html').on('change', browserSync.reload())
+
+
+});
+
+gulp.task('default',['sass','serve']);
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
-});
+// gulp.task('sass:watch', function () {
+//   gulp.watch('./scss/**/*.scss', ['sass']);
+// });
